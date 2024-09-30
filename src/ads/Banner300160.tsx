31a309ba68d972bd14da8e174a3293c94595f0dc
sushil70 <69754620+sushil70@users.noreply.github.com>
2024-10-01 00:31:44 +0530

    banner fix

--- src/ads/Banner300160.tsx
+++ src/ads/Banner300160.tsx
@@ -17,7 +17,8 @@ export default function Banner300160() {
       const conf = document.createElement("script");
       const script = document.createElement("script");
       script.type = "text/javascript";
-      script.src = `"//www.topcreativeformat.com/44f4f26a4201f53bc307c912b35a651f/invoke.js"`;
+      script.src =
+        "//www.topcreativeformat.com/44f4f26a4201f53bc307c912b35a651f/invoke.js";
       conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
 
       banner.current.append(conf);
